4df100b0e4170f30438cd24fb193e2521af1bb7e
Aditya Kulkarni <kulkarniaditya2099@gmail.com>
2021-03-24 17:15:49 +0530

    Added Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import {Container} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import TodoForm from './Components/TodoForm';
+import Todos from './Components/Todos';
 
 function App() {
+    const [todos, setTodos] = useState([])
+    useEffect(() => {
+        const localTodos = localStorage.getItem("todos")
+        console.log({localStorage});
+        if(localTodos){
+            setTodos(JSON.parse(localTodos))
+        }
+    }, [])
+
+    const addTodos = async todo => {
+        setTodos([...todos, todo])
+    }
+    useEffect(() =>{
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos])
+
+    const markComplete = id => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+        <TodoForm addTodos={addTodos}/>
+        <Todos todos={todos} markComplete={markComplete}/>
+        
+    </Container>
   );
 }
 
